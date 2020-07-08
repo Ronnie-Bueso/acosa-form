b1c707cef738130b60b75d6e4fe70ecdcc4401de
Ronnie Bueso <52080194+Ronnie-Bueso@users.noreply.github.com>
2020-07-08 16:13:28 -0600

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-        var id1 = document.getElementById("area");
-        var id2 = document.getElementById("puestos");
+        var id1 = document.getElementById("ciudad");
+        var id2 = document.getElementById("area");
         
         if (id1.addEventListener) {    
             id1.addEventListener("change", cambiar);
@@ -9,7 +9,6 @@
 
         function cambiar() {
             for (var i = 0; i < id2.options.length; i++)
-            
             if(id2.options[i].getAttribute("codigo") == id1.options[id1.selectedIndex].getAttribute("codigo")){
                 id2.options[i].style.display = "block";
             }else{
@@ -19,3 +18,27 @@
         }
 
         cambiar();
+
+        var id3 = document.getElementById("area");
+        var id4 = document.getElementById("puestos");
+        
+        if (id3.addEventListener) {    
+            id3.addEventListener("change", cambiar);
+        } else if (id3.attachEvent) { 
+            id3.attachEvent("change", cambiar); 
+        }
+
+        function cambiar() {
+            for (var i = 0; i < id4.options.length; i++)
+            
+            if(id4.options[i].getAttribute("codigo") == id3.options[id3.selectedIndex].getAttribute("codigo")){
+                id4.options[i].style.display = "block";
+            }else{
+                id4.options[i].style.display = "none";
+            }        
+            id4.value = "";
+        }
+
+        cambiar();
+
+        
